f7e4eb4f07e61551f1869b3c593fee992aacad77
nforne <mnforne@gmail.com>
2022-01-06 13:23:03 -0500

    add some more validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,7 +1,7 @@
 class Employee < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
-  # validates :hourly_rate, numericality: {only_integer: true, in 40..200}
+  validates :hourly_rate, numericality: {in: 40..200}
   validates :store_id, presence: true
   belongs_to :store
 end
--- lib/store.rb
+++ lib/store.rb
@@ -1,12 +1,12 @@
 class Store < ActiveRecord::Base
+  has_many :employees
   validates :name, length: { minimum: 3 }
-  # validates :annual_revenue, integer: { in 0..Integer::INFINITY }
-  # validates :store_must_carry_apparel
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
   # def store_must_carry_apparel
   #   if !self.womens_apparel && !self.mens_apparel
   #     errors.add(:store_must_carry_apparel, "Store must carry at least mens or womens apparel")
   #   end
   # end
+  # validates_with store_must_carry_apparel,
   
-  has_many :employees
 end
