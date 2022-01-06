0e45432afb59383fa1c1a3becab9159acade8d10
nforne <mnforne@gmail.com>
2022-01-06 09:49:52 -0500

    add three stores and load stores that carry men apprel, exercise 4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,19 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+
+surrey =  Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+puts Store.count
+
+whistler =  Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+puts Store.count
+
+yaletown =  Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+puts Store.count
+
+@mens_stores = Store.where(mens_apparel: true).order(created_at: :desc)
+p @mens_stores
